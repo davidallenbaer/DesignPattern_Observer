eafd9b51d4a5278b04c8c03efe6b6bbe8f678dd1
davidallenbaer <davidallenbaer@gmail.com>
2021-05-16 18:55:44 -0400

    Update 1

--- DesignPattern_Observer/Program.cs
+++ DesignPattern_Observer/Program.cs
@@ -12,11 +12,11 @@ namespace DesignPattern_Observer
         {
             //Create a Product with Out Of Stock Status
             Subject RedMI = new Subject("Red MI Mobile", 10000, "Out Of Stock");
-            //User Anurag will be created and user1 object will be registered to the subject
+            //User Dave will be created and user1 object will be registered to the subject
             Observer user1 = new Observer("Dave", RedMI);
-            //User Pranaya will be created and user1 object will be registered to the subject
+            //User Tom will be created and user1 object will be registered to the subject
             Observer user2 = new Observer("Tom", RedMI);
-            //User Priyanka will be created and user3 object will be registered to the subject
+            //User Mitch will be created and user3 object will be registered to the subject
             Observer user3 = new Observer("Mitch", RedMI);
 
             Console.WriteLine("Red MI Mobile current state : " + RedMI.getAvailability());
